955a846fbd8a1b7eec7f2b49cd20c24644ef6bc3
gyuuuu <kai9702@gmail.com>
2021-04-29 02:39:11 +0900

    add validation of class

--- backend/src/product/entites/product.entity.ts
+++ backend/src/product/entites/product.entity.ts
@@ -1,3 +1,4 @@
+import { IsDate, IsString } from 'class-validator';
 import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export class product {
@@ -5,8 +6,10 @@ export class product {
   product_id: number;
 
   @Column({ type: 'varchar' })
+  @IsString()
   product_name: string;
 
   @Column({ type: 'date' })
+  @IsDate()
   expired_data: string;
 }
