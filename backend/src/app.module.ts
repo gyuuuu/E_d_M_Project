3c8dc1c0e54be14e97e085017bc822d46bf7ee46
gyuuuu <kai9702@gmail.com>
2021-04-29 02:31:22 +0900

    delete unused file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,15 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { envConfig } from 'config/env.config';
 import { ormConfig } from 'config/ormConfig';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(envConfig()),
     TypeOrmModule.forRoot(ormConfig()),
+    ProductModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
