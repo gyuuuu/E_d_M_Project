ed8802a62c104050cf2d7bc631c632230b5967dd
gyuuuu <kai9702@gmail.com>
2021-04-29 02:57:16 +0900

    func find all product

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { product } from './entites/product.entity';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([product])],
   controllers: [ProductController],
   providers: [ProductService],
 })
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { product } from './entites/product.entity';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(product)
+    private readonly productRepository: Repository<product>,
+  ) {}
+
+  async findall() {
+    return await this.productRepository.find({
+      order: { expired_data: 'DESC' },
+    });
+  }
+}
